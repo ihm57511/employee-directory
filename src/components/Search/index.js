c433b07d859a344d47e9c052dffd478405bb5927
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-10 11:11:59 -0400

    added search field

---
+++ src/components/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <>
+      <div class="input-group">
+        <span class="input-group-text">With textarea</span>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+    </>
+  );
+}
+
+export default Search;
