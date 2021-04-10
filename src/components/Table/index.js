0ddb04e04b7acb05ea8d5020e323cd3c419e33c4
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-10 14:33:51 -0400

    corrected map to display all employees from api call

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,31 +1,47 @@
-import React from 'react';
-import employees from '../../employees.json';
+import React, {Component} from 'react';
+import API from '../../utils/API';
 
-const Table = () => {
-  return(
-    <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Image</th>
-      <th scope="col">Name</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Email</th>
-      <th scope="col">DOB</th>
-    </tr>
-  </thead>
-  <tbody>
-    {employees.map(employee => (
-    <tr>
-      <td>{<img height="60" src={employee.image} alt={employee.name} />}</td>
-      <td>{employee.name}</td>
-      <td>{employee.phone}</td>
-      <td>{employee.email}</td>
-      <td>{employee.dob}</td>
-    </tr>
-    ))}
-  </tbody>
-</table>
-  )
+class Table extends Component {
+  state = {
+    employees: [],
+  };
+
+  componentDidMount() {
+    this.searchEmployee();
+  };
+
+  searchEmployee = () => {
+    API.search()
+      .then(res => this.setState({ employees: res.data.results }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.employees.map(employee => (
+            <tr>
+              <td>{<img src={employee.picture.thumbnail} alt={employee.name} />}</td>
+              <td>{employee.name.first} {employee.name.last}</td>
+              <td>{employee.phone}</td>
+              <td>{employee.email}</td>
+              <td>{employee.dob.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
 }
 
 export default Table;
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = 'https://randomuser.me/api/?results=35&nat=us';
 
 export default {
-  search: function(query) {
+  search: function() {
     return axios.get(url);
   }
 };
