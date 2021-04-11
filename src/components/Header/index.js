4d7cc89631d6bed9149fff481f23f6d90fa7b22a
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-11 16:21:44 -0400

    styled header and search bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ const Header = () => {
     <>
       <header>
         <h1>Employee Directory</h1>
+        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
       </header>
     </>
   )
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Search = (props) => {
   return (
     <>
-      <div className="input-group">
+      <div className="input-group d-flex justify-content-center">
         <input className="form-control col-2"
         onChange={event => props.handleSearch(event)}
-        type='search'
+        type='search' placeholder="Search"
         />
       </div>
     </>
